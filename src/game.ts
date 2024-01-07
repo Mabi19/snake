dcc9f2cd2527daf860348aff93bf280fa07c606f
Mabi19 <mateuszbartnik33@gmail.com>
2024-01-07 20:55:53 +0100

    Add fruit spawning
    not rendered yet

--- src/game.ts
+++ src/game.ts
@@ -1,34 +1,20 @@
 import { TableRenderer } from "./Renderer";
 import { Vector } from "./Vector";
+import { settings } from "./settings";
 
 let snake: Vector[] = [];
 let direction = new Vector(0, 0);
 let directionBuffer: Vector[] = [];
+let fruit: Vector[] = [];
 
 export function setup(renderer: TableRenderer) {
     snake = [new Vector(2, 0), new Vector(1, 0), new Vector(0, 0)];
     direction = new Vector(0, 0);
     directionBuffer = [];
+    fruit = [];
+    spawnFruit();
+
     // initial render
-    renderer.render(snake, []);
-}
-
-export function runTick(renderer: TableRenderer) {
-    // process inputs
-    if (directionBuffer.length > 0) {
-        direction = directionBuffer.shift()!;
-    }
-
-    // if snake is not moving, skip
-    if (direction.x == 0 && direction.y == 0) {
-        return;
-    }
-
-    // add head and remove tail
-    const newHead = snake[0].add(direction);
-    snake.unshift(newHead);
-    snake.pop();
-
     renderer.render(snake, []);
 }
 
@@ -73,4 +59,42 @@ export function handleInput(ev: KeyboardEvent) {
     }
 
     document.querySelector("#debug")!.textContent = JSON.stringify(directionBuffer);
+}
+
+function spawnFruit() {
+    const takenTiles = snake.concat(fruit);
+    const freeTiles = [];
+    for (let testX = 0; testX < settings.boardSize.x; testX++) {
+        for (let testY = 0; testY < settings.boardSize.y; testY++) {
+            if (!takenTiles.find(({ x, y }) => testX == x && testY == y)) {
+                freeTiles.push(new Vector(testX, testY));
+            }
+        }
+    }
+    // When there are not enough tiles to spawn fruit, don't spawn it.
+    const fruitToSpawn = Math.max(settings.fruitCount - fruit.length, freeTiles.length);
+    for (let i = 0; i < fruitToSpawn; i++) {
+        const tileIndex = Math.floor(Math.random() * freeTiles.length);
+        const [tile] = freeTiles.splice(tileIndex, 1);
+        fruit.push(tile);
+    }
+}
+
+export function runTick(renderer: TableRenderer) {
+    // process inputs
+    if (directionBuffer.length > 0) {
+        direction = directionBuffer.shift()!;
+    }
+
+    // if snake is not moving, skip
+    if (direction.x == 0 && direction.y == 0) {
+        return;
+    }
+
+    // add head and remove tail
+    const newHead = snake[0].add(direction);
+    snake.unshift(newHead);
+    snake.pop();
+
+    renderer.render(snake, []);
 }
--- src/settings.ts
+++ src/settings.ts
@@ -3,9 +3,11 @@ import { Vector } from "./Vector";
 export interface GameSettings {
     tickLength: number;
     boardSize: Vector;
+    fruitCount: number;
 }
 
 export const settings: GameSettings = {
     tickLength: 250,
     boardSize: new Vector(10, 10),
+    fruitCount: 1,
 };
